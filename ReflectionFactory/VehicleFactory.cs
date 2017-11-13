a32b45d36b623c35e7abd84f6f5a0ad447d98c89
srzeppa <srzeppa@sigma.ug.edu.pl>
2017-11-13 13:39:38 +0100

    initial commit

---
+++ ReflectionFactory/VehicleFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ReflectionFactory.Model;
+using System.Reflection;
+
+namespace ReflectionFactory
+{
+    public abstract class VehicleFactory
+    {
+        public static BaseVehicle CreateVehicle<T>() where T : BaseVehicle
+        {
+            return Activator.CreateInstance<T>();
+        }
+
+        public static BaseVehicle CreateVehicle(string name)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            var type = assembly.GetType(name).FullName;
+            return (BaseVehicle)Activator.CreateComInstanceFrom(assembly.Location, type).Unwrap();
+        }
+    }
+}
